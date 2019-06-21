3d41933b6b8c378d9105d233b35d9ae59dc95040
Scoopapa <pccompositor@aim.com>
2019-06-21 05:46:54 -0500

    redo 2

---
+++ precache-manifest.7cb6213bbe570566036f6ef09eeea9fc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f451facf91f4e3e5b5caf78b6dbdb224",
+    "url": "/Space-X-Launch-Page/index.html"
+  },
+  {
+    "revision": "22390f097aaf2e94a075",
+    "url": "/Space-X-Launch-Page/static/css/main.f9c67ae1.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/Space-X-Launch-Page/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "22390f097aaf2e94a075",
+    "url": "/Space-X-Launch-Page/static/js/main.69eff1d3.chunk.js"
+  },
+  {
+    "revision": "fcbffc12463ed37a202b",
+    "url": "/Space-X-Launch-Page/static/js/runtime~main.bfaa3d41.js"
+  }
+]);
